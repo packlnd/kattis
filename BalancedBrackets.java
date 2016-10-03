482f08a4addbcf7b2d95ecdf6c4b8916d7507585
Patrik Ackland <patrikackland@gmail.com>
2016-10-03 13:02:10 +0200

    Balanced brackets

---
+++ BalancedBrackets.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class BalancedBrackets {
+
+  private static boolean isOpenBracket(char c) {
+    return c == '{' || c == '[' || c == '(';
+  }
+
+  private static boolean isMatching(char c1, char c2) {
+    return (c1 == '(' && c2 == ')') || (c1 == '[' && c2 == ']') || (c1 == '{' && c2 == '}');
+  }
+
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int n = Integer.parseInt(in.nextLine());
+    for (int t=0; t<n; ++t) {
+      String s = in.nextLine();
+      Stack<Character> stack = new Stack<Character>();
+      for (int i=0; i<s.length(); ++i) {
+        if (isOpenBracket(s.charAt(i))) {
+          stack.push(s.charAt(i));
+          if (i==s.length()-1)
+            System.out.println("NO");
+          continue;
+        }
+        if (stack.isEmpty()) {
+          System.out.println("NO");
+          break;
+        }
+        char c = stack.pop();
+        if (!isMatching(c, s.charAt(i))) {
+          System.out.println("NO");
+          break;
+        }
+        if (i==s.length()-1) {
+          if (stack.isEmpty())
+            System.out.println("YES");
+          else
+            System.out.println("NO");
+        }
+      }
+    }
+  }
+}
