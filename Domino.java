7bc2e744e3f08a4930e5d8ff7d0beea11701c0e7
Patrik Ackland <patrikackland@gmail.com>
2016-10-05 14:16:42 +0200

    Domino

---
+++ Domino.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Domino {
+  public static class Brick {
+    String dir;
+
+  }
+  public static void main(String[] args) {
+    Scanner in = new Scanner(System.in);
+    int M = in.nextInt();
+    int N = in.nextInt();
+
+  }
+}
